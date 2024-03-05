85692a98364bc0ca0e99052f84be495a4a5949a5
MahtaabAsadi <162023849+MahtaabAsadi@users.noreply.github.com>
2024-03-05 18:34:45 +0330

    added task

---
+++ Tamrin Jalase 02/Task01.py
@@ -0,0 +1,6 @@
+# voroudi:nomre mianterm , nomre payanterm  khorouji:jamE do nomre , miangin do nomre. 
+mianterm = 16.5
+payanterm = 17.75
+print(mianterm+payanterm)
+print(mianterm+payanterm/2)
+
---
+++ Tamrin Jalase 02/main.py
@@ -0,0 +1,9 @@
+# 3 ta moteghayer : firstname-lastname-age  va harkodam print beshan.
+# string
+firstname = 'Mahtaab'
+lastname = 'Asadi'
+# integer
+age = 19
+print(firstname)
+print(lastname)
+print(age)
