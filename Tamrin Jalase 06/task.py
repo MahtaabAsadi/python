e426c187421c1dd0302c08cbbb211c13ee559b9b
MahtaabAsadi <162023849+MahtaabAsadi@users.noreply.github.com>
2024-04-29 16:51:24 +0330

    added J6

---
+++ Tamrin Jalase 06/task.py
@@ -0,0 +1,8 @@
+x = int(input('enter your number: '))
+
+Sum = 1
+
+for i in range(1, x+1):
+    Sum = Sum * i
+
+print(Sum)
---
+++ Tamrin Jalase 06/task01.py
@@ -0,0 +1,8 @@
+x = int(input('enter your number: '))
+
+Sum = 1
+
+for i in range(x):
+    Sum = Sum * (i+1)
+
+print(Sum)
